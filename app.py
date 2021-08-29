0556730f792f043131a18cd21266089f70b99a3b
Aayog Koirala <1aayogkoirala@gmail.com>
2021-08-29 10:01:13 -0400

    Create app.py
    https://github.com/danasiciliano/The-Autocratic-Aggregators/tree/main

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from helper import get_questions, get_info_from_csv, get_links_from_service
+
+app = Flask(__name__)
+
+@app.route("/")
+def form():
+    questions = get_questions("questions.txt")
+    return render_template("form.html", questions=questions)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    answers = ""
+    req = request.form.get("0", False)
+    resources_dict = get_info_from_csv("resources.csv")
+    links = get_links_from_service(resources_dict, req)
+    print(links)
+    return render_template("result.html", links=list(links))
+
+
+def main():
+    app.run(debug=True)
+
+if __name__=="__main__":
+    main()
+
+
+
+answers_to_links = {
+    [1 ,2, 3] : "SNAP"
+}
